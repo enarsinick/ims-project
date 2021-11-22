932bca3cf3dd8a97100f7ee2bdc2e1915a2d546d
enarsinick <nxdmedia@googlemail.com>
2021-11-22 11:04:10 +0000

    NGS-20 #comment Added SQL tables and dummy data to main and test folders

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,42 @@
 DROP TABLE IF EXISTS `customers`;
 
+-- customer table
 CREATE TABLE IF NOT EXISTS `customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+	`customer_id` int AUTO_INCREMENT,
+    `first_name` varchar(255) NOT NULL,
+    `last_name` varchar(255) NOT NULL,
+    PRIMARY KEY (`customer_id`)
+);
+
+DROP TABLE IF EXISTS `products`;
+
+-- products/items table
+CREATE TABLE IF NOT EXISTS `products` (
+	`product_id` int AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `price` decimal(5,2) NOT NULL,
+    PRIMARY KEY (`product_id`)
+);
+
+DROP TABLE IF EXISTS `orders`;
+
+-- orders table 
+CREATE TABLE IF NOT EXISTS `orders` (
+	`order_id` int AUTO_INCREMENT, 
+    `fk_customer_id` int NOT NULL, 
+    PRIMARY KEY (`order_id`),
+    FOREIGN KEY (`fk_customer_id`) REFERENCES customers(`customer_id`)
+);
+
+DROP TABLE IF EXISTS `order_items`;
+
+-- order items joining table
+CREATE TABLE IF NOT EXISTS `order_items` (
+	`order_items_id` int AUTO_INCREMENT, 
+    `fk_order_id` int NOT NULL, 
+    `fk_product_id` int NOT NULL, 
+    `order_quantity` int NOT NULL, 
+    PRIMARY KEY (`order_items_id`), 
+    FOREIGN KEY (`fk_order_id`) REFERENCES orders(`order_id`), 
+    FOREIGN KEY (`fk_product_id`) REFERENCES products(`product_id`)
 );
